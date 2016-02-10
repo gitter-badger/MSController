fd0b943679b5c1157d33b501206a95b3649aa9c7
Dan Stewart <danielandrewstewart@gmail.com>
2016-02-10 15:55:01 +0000

    Add ability to set email importance level
    Email importance can now be set via an optional parameter.
    Example: sendMail(subject, body, recipient, importance: 2);
    0=low, 1=normal, 2=high.

--- MSControl/OutlookHandler.cs
+++ MSControl/OutlookHandler.cs
@@ -15,9 +15,10 @@ namespace MSController
         /// <param name="subject">Subject line of the email.</param>
         /// <param name="body">Body text of the email.</param>
         /// <param name="recipient">Single recipient of the email</param>
-        public void sendMail(string subject, string body, string recipient)
+        /// <param name="importance">The importance level of the email. Can be low (0), normal (1) or high (2).</param>
+        public void sendMail(string subject, string body, string recipient, int importance=1)
         {
-            sendMail(subject, body, new List<string>() { recipient }, new List<string>());
+            sendMail(subject, body, new List<string>() { recipient }, new List<string>(), importance);
         }
 
         /// <summary>
@@ -26,9 +27,10 @@ namespace MSController
         /// <param name="subject">Subject line of the email.</param>
         /// <param name="body">Body text of the email.</param>
         /// <param name="recipients">List of recipients of the email.</param>
-        public void sendMail(string subject, string body, List<string> recipients)
+        /// <param name="importance">The importance level of the email. Can be low (0), normal (1) or high (2).</param>
+        public void sendMail(string subject, string body, List<string> recipients, int importance=1)
         {
-            sendMail(subject, body, recipients, new List<string>());
+            sendMail(subject, body, recipients, new List<string>(), importance);
         }
 
         /// <summary>
@@ -38,9 +40,10 @@ namespace MSController
         /// <param name="body">Body text of the email.</param>
         /// <param name="recipeint">Single recipient of the email.</param>
         /// <param name="attachmentPath">Single attachment path of the email.</param>
-        public void sendMail(string subject, string body, string recipeint, string attachmentPath)
+        /// <param name="importance">The importance level of the email. Can be low (0), normal (1) or high (2).</param>
+        public void sendMail(string subject, string body, string recipeint, string attachmentPath, int importance=1)
         {
-            sendMail(subject, body, new List<string>() { recipeint }, new List<string>() { attachmentPath });
+            sendMail(subject, body, new List<string>() { recipeint }, new List<string>() { attachmentPath }, importance);
         }
 
         /// <summary>
@@ -50,9 +53,10 @@ namespace MSController
         /// <param name="body">Body text of the email.</param>
         /// <param name="recipients">List of recipients of the email.</param>
         /// <param name="attachmentPath">Single attachment path of the email.</param>
-        public void sendMail(string subject, string body, List<string> recipients, string attachmentPath)
+        /// <param name="importance">The importance level of the email. Can be low (0), normal (1) or high (2).</param>
+        public void sendMail(string subject, string body, List<string> recipients, string attachmentPath, int importance=1)
         {
-            sendMail(subject, body, recipients, new List<string>() { attachmentPath });
+            sendMail(subject, body, recipients, new List<string>() { attachmentPath }, importance);
         }
 
         /// <summary>
@@ -62,9 +66,10 @@ namespace MSController
         /// <param name="body">Body text of the email.</param>
         /// <param name="recipient">Single recipient of the email.</param>
         /// <param name="attachmentPaths">List of attachment paths of the email.</param>
-        public void sendMail(string subject, string body, string recipient, List<string> attachmentPaths)
+        /// <param name="importance">The importance level of the email. Can be low (0), normal (1) or high (2).</param>
+        public void sendMail(string subject, string body, string recipient, List<string> attachmentPaths, int importance=1)
         {
-            sendMail(subject, body, new List<string>() { recipient }, attachmentPaths);
+            sendMail(subject, body, new List<string>() { recipient }, attachmentPaths, importance);
         }
 
         /// <summary>
@@ -74,7 +79,8 @@ namespace MSController
         /// <param name="body">Body text of the email.</param>
         /// <param name="recipients">List of recipients of the email.</param>
         /// <param name="attachmentPaths">List of the attachment paths of the email.</param>
-        public void sendMail(string subject, string body, List<string> recipients, List<string> attachmentPaths)
+        /// <param name="importance">The importance level of the email. Can be low (0), normal (1) or high (2).</param>
+        public void sendMail(string subject, string body, List<string> recipients, List<string> attachmentPaths, int importance=1)
         {
             try
             {
@@ -82,15 +88,27 @@ namespace MSController
                 Outlook.MailItem mailItem = (Outlook.MailItem)app.CreateItem(Outlook.OlItemType.olMailItem);
                 mailItem.Subject = subject;
                 mailItem.Body = body;
-                mailItem.To = String.Join("; ", recipients.ToArray());
+                mailItem.To = string.Join("; ", recipients.ToArray());
 
                 foreach (string attachment in attachmentPaths)
                     mailItem.Attachments.Add(attachment);
 
-                mailItem.Importance = Outlook.OlImportance.olImportanceNormal;
+                switch (importance)
+                {
+                    case 0:
+                        mailItem.Importance = Outlook.OlImportance.olImportanceLow;
+                        break;
+                    case 1:
+                        mailItem.Importance = Outlook.OlImportance.olImportanceNormal;
+                        break;
+                    case 2:
+                        mailItem.Importance = Outlook.OlImportance.olImportanceHigh;
+                        break;
+                }
+                
                 mailItem.Display(false);
 
-                ((Outlook._MailItem)mailItem).Send();
+                mailItem.Send();
             }
             catch (Exception ex)
             {
